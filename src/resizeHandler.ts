0db4c2bb2abec6b114e5d74f8b10837222486b5a
Ganesh Adapa <ganesh.adapa@mysense.ai>
2020-01-07 21:32:05 +0000

    feat: 🎸 resizehandler function basic version is added
    BREAKING CHANGE: n

--- src/resizeHandler.ts
+++ src/resizeHandler.ts
@@ -1,5 +1,5 @@
 import { S3Handler } from 'aws-lambda';
 
-export const imageCreateOrUpdateNotifier: S3Handler  = (event) => {
+export const resizeHandler: S3Handler  = (event) => {
     console.log('event: ', JSON.stringify(event));
     };
