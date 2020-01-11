fd60070780d396fd403dd282d618352f76350002
Ganesh Adapa <ganesh.adapa@mysense.ai>
2020-01-11 11:42:47 +0000

    test: 💍 updated domain name in test cases

--- src/tests/endpoint/health.test.js
+++ src/tests/endpoint/health.test.js
@@ -2,7 +2,8 @@ const got = require('got');
 
 describe('Health end point functionality', () => {
     it('should return success response', async() => {
-            const response = await got('https://api.aspirex.in/health');
+            // todo: how to extractout the DNS so that no need to change it whenever there is a change in domain name
+            const response = await got('https://sampleapp.aspirex.in/health');
             console.log(response.body);
             expect(response.body).toBeTruthy();
     });
