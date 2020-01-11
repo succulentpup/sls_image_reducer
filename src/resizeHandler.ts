d509e81d53e614b6dcc7eb48cd6b1a92f63a70a9
Ganesh Adapa <ganesh.adapa@mysense.ai>
2020-01-11 08:31:19 +0000

    feat: 🎸 few issues got fixed in resizehandler & worker
    BREAKING CHANGE: n

--- src/resizeHandler.ts
+++ src/resizeHandler.ts
@@ -2,8 +2,12 @@ import { S3Handler } from 'aws-lambda';
 
 import { resize } from './resizeWorker';
 
+const { UPLOADS_FOLDER, THUMBNAILS_FOLDER } = process.env;
+
 export const resizeHandler: S3Handler  = async (event) => {
     const bucket = event.Records[0].s3.bucket.name;
     const key = event.Records[0].s3.object.key;
-    await resize(bucket, key);
+    // todo: assert environment variables
+    // if (!UPLOADS_FOLDER || !THUMBNAILS_FOLDER) return new Error('Either UPLOADS_FOLDER or THUMBNAILS_FOLDER is missed');
+    await resize(bucket, key, UPLOADS_FOLDER as string, THUMBNAILS_FOLDER as string);
     };
--- src/resizeWorker.ts
+++ src/resizeWorker.ts
@@ -1,15 +1,13 @@
 import { S3 } from 'aws-sdk';
 import { GetObjectOutput, PutObjectOutput } from 'aws-sdk/clients/s3';
-import * as Jimp from 'jimp';
+import Jimp from 'jimp';
 
 const s3 = new S3();
 const QUALITY_PERCENTATE = 100;
 
-export const resize = async (bucketName: string, key: string) => {
-    console.log('resizer is called');
-    console.log('bucketName: ', bucketName);
-    console.log('key: ', key);
-    const newKey = replacePrefix(key);
+export const resize = async (bucketName: string, key: string, uploadsFolder: string, thumbnailsFolder: string) => {
+    const newKey = replacePrefix(key, uploadsFolder, thumbnailsFolder);
+    console.log('newKey:: ', newKey);
     const height = 512;
     return getS3Object(bucketName, key)
         .then((data) => resizer(data.Body as Buffer, height))
@@ -17,19 +15,22 @@ export const resize = async (bucketName: string, key: string) => {
 
 };
 
-const replacePrefix = (key: string): string => {
-    const uploadPrefix = 'uploads/';
-    const thumbnailsPrefix = 'thumbnails/';
+const replacePrefix = (key: string, uploadsFolder: string, thumbnailsFolder: string): string => {
+    const uploadPrefix = uploadsFolder;
+    const thumbnailsPrefix = thumbnailsFolder;
+    console.log(`key: ${key} -- uploadPrefix: ${uploadPrefix} -- thumbnailsPrefix: ${thumbnailsPrefix}`);
     return key.replace(uploadPrefix, thumbnailsPrefix);
 };
 
-const resizer = (data: Buffer, height: number): Promise<Buffer> =>
-    Jimp.read(data)
+const resizer = (data: Buffer, height: number): Promise<Buffer> => {
+    console.log('inside resizer', data);
+    return Jimp.read(data)
         .then((image) =>
             image
                 .resize(Jimp.AUTO, height)
                 .quality(QUALITY_PERCENTATE)
                 .getBufferAsync(Jimp.MIME_JPEG));
+};
 
 const getS3Object = (bucket: string, key: string): Promise<GetObjectOutput> =>
     s3.getObject({
